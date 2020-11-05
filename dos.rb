7dd3ec65201740cdb11e79097b6615b1edec875c
Pablo <pablochavezing@gmail.com>
2020-11-05 14:55:12 -0800

    Update_TEST_V2

--- dos.rb
+++ dos.rb
@@ -1,7 +1,6 @@
-require 'uri'
+
 require 'net/http'
 require 'json'
-require 'openssl'
 
     def request(address, api_key)
         url = URI(address+"&api_key="+api_key)
@@ -11,29 +10,21 @@ require 'openssl'
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         request = Net::HTTP::Get.new(url)
-        request["cache-control"] = 'no-cache'
-        request["Postman-Token"] = '251e4a0e-ad0f-405c-9792-08d049ab496b'
 
         response = http.request(request)
         JSON.parse response.read_body
     end
+    
     body = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY", "2y4Kfc1QTMpM5TYq4VkPn0Fd652aJpbp516DxTDD")
-    body = body["photos"][0..5]
+    body = body["photos"][0..5]                     
 
 
     def build_web_page(body)
         html = "<html>\n<title>API Nasa</title>\n<head>\n</head>\n<body>\n<h1>API Nasa<h1>\n<ul>\n"
-        body.collect {|img|
-        html += "\t<li><img src=\"#{img["img_src"]}\"></li>\n"}    
+        body.collect {|img|html += "\t<li><img src=\"#{img["img_src"]}\"></li>\n"}    
         html += "</ul>\n</body>\n</html>"
-        File.write('index.html', html)
+        File.write('Ruby_API_NASA3.html', html)
     end
 
-
-    def photo_count(body)
-        hash = {}
-        body.collect{|cam| hash[cam['camera']['name']] = 1 + hash[cam['camera']['name']].to_i}
-        return hash
-    end
-puts photo_count(body)
-build_web_page(body)
+    build_web_page(body)
+    puts "Pagina WEB creada"
